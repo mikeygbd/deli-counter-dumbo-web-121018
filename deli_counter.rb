b8c235119f9ed4262ad9d45c1e4b782db967841c
Michael Soares <mikey.soares@gmail.com>
2018-12-02 05:43:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+      puts "The line is currently empty."
+    else
+      positions = []
+      katz_deli.each_with_index do |customer, i|
+        positions.push("#{i + 1}. #{customer}")
+
+      end
+      puts "The line is currently: #{positions.join(' ')}"
+    end
+  end
